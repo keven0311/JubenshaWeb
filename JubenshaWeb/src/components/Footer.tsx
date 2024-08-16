01934880b135c5f6982f5e9cb0c239bf9b51b26c
kaichong lin <coco.keven@gmail.com>
2024-08-16 13:34:03 -0500

    footer updated with correct informations

--- JubenshaWeb/src/components/Footer.tsx
+++ JubenshaWeb/src/components/Footer.tsx
@@ -4,12 +4,12 @@ function Footer() {
   return (
     <div className="footer-div">
       <div className='footer-left'>
-        <img className='wechat-sq-code-img' src='/wechat-sq-code.png' alt='Wechat SQ code'/>
+        <img className='wechat-sq-code-img' src='/wechat-sq-code-new.png' alt='Wechat SQ code'/>
       </div>
       <div className='footer-right'>
-        <p>Tel:(222)333-4444</p>
-        <p>Address: 2531 Chain Bridge Rd,</p>
-        <p>2nd Fllor, Vienna, VA, 22181</p>
+        <p>Tel:(202)701-9391</p>
+        <p>Address: 450 Maple Ave,</p>
+        <p>E Suit #310, Vienna, VA</p>
         <small>@2024 Infinity Space INC.</small>
       </div>
     </div>
